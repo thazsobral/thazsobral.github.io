95c97d067a42d81171d4b07439c20168265d1744
thazsobral <30708148+thazsobral@users.noreply.github.com>
2022-03-14 23:15:54 -0300

    Update fixedHeaderTop.js

--- scripts/fixedHeaderTop.js
+++ scripts/fixedHeaderTop.js
@@ -7,7 +7,6 @@ var stick = header.offsetTop
 
 function addClassHeader() {
     if(window.pageYOffset > stick) {
-        // alert('chegou')
         header.setAttribute('class', 'stick')
     } else {
         header.classList.remove('stick')
