730776b56fb6a43d0ac90de1fcb1da2c80f441b5
thazsobral <thazsobral@gmail.com>
2022-11-20 12:47:04 -0300

    fix: adjusted functions

--- assets/scripts/repos.js
+++ assets/scripts/repos.js
@@ -1,35 +1,35 @@
 fetch("https://api.github.com/users/thazsobral/repos")
-  .then (function (response) {
-    return response.json()
+  .then (function(response) {
+    return response.json();
   })
   .then (function(data) {
-    setPortifolio(data)
+    setPortifolio(data);
   })
-  .catch (function () {
-    console.error("Error")
-  })
+  .catch (function(error) {
+    console.error(`Error: ${error}`);
+  });
 
-function setPortifolio (repos) {
-  const writeSpace = getWriteSpace("portfolio-github")
-  var id = 0
+function setPortifolio(repos) {
+  const writeSpace = getWriteSpace("portfolio-github");
+  var id = 0;
   repos.forEach((repo) => {
     // elementos que começam com - não devem ser exibidos
 
-    if(repo.name !== "ThazSobral.github.io" & repo.name[0] !== "-") {
-      id ++
-      var nameProject = document.createTextNode((id) + " - " + repo.name)
-      var linkProject = document.createElement("a")
-      var repoTopic = document.createElement("h2")
+    if(repo.name !== "thazsobral.github.io" & repo.name[0] !== "-") {
+      id ++;
+      var nameProject = document.createTextNode((id) + " - " + repo.name);
+      var linkProject = document.createElement("a");
+      var repoTopic = document.createElement("h2");
 
-      linkProject.setAttribute("href", repo.html_url)
+      linkProject.setAttribute("href", repo.html_url);
       
-      linkProject.appendChild(nameProject)
-      repoTopic.appendChild(linkProject)
-      writeSpace.appendChild(repoTopic)
+      linkProject.appendChild(nameProject);
+      repoTopic.appendChild(linkProject);
+      writeSpace.appendChild(repoTopic);
     }
   })
 }
 
-function getWriteSpace (id) {
-  return document.getElementById(id)
+function getWriteSpace(id) {
+  return document.getElementById(id);
 }
