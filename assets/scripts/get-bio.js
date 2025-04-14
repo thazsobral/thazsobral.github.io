a0b0be2980eb365526f8884f729aa6bf3c4f6886
thazsobral <30708148+thazsobral@users.noreply.github.com>
2025-04-14 14:12:30 -0300

    Update get-bio.js

--- assets/scripts/get-bio.js
+++ assets/scripts/get-bio.js
@@ -10,29 +10,29 @@ fetch("https://raw.githubusercontent.com/thazsobral/thazsobral/main/README.md")
     });
 
 function setBio(bio) {
-    let startSignal = "###";
-    let endSignal = "👋";
-
-    let [startPoint, endPoint] = startAndEndPoints(bio, startSignal, endSignal);
-    
-    let title = document.createElement("h3");
-    title.innerText = bio.slice((startPoint+3), (endPoint+2));
-    
-    startSignal = "- ";
-    endSignal = "---";
-    
-    [startPoint, endPoint] = startAndEndPoints(bio, startSignal, endSignal);
-    
-    let text = document.createElement("p");
-    text.innerText = bio.slice(startPoint, endPoint);
+    let stringPoint = "&#8203;";
+    let lengthStringPoint = stringPoint.length;
 
     let writeSpace = document.getElementById("bio-resume");
-    writeSpace.appendChild(title);
-    writeSpace.appendChild(text);
+
+    let positionsString = findSubstringPositions(bio, stringPoint);
+
+    if (positionsString.length > 1) {
+        let text = document.createElement("p");
+        text.innerText = bio.slice(positionsString[0]+lengthStringPoint, positionsString[1]);
+    
+        writeSpace.appendChild(text);
+    }
 }
 
-function startAndEndPoints(text, startSignal, endSignal) {
-    let startPoint = text.indexOf(startSignal);
-    let endPoint = text.indexOf(endSignal);
-    return [startPoint, endPoint];
+function findSubstringPositions(mainString, substring) {
+  const positions = [];
+  let indice = mainString.indexOf(substring);
+
+  while (indice !== -1) {
+    positions.push(indice);
+    indice = mainString.indexOf(substring, indice + 1);
+  }
+
+  return positions;
 }
